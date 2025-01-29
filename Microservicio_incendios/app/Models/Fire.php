18a58f049b1c46bb703d7dd5f9758358de290b8e
acevedovv <juacevedov@unal.edu.co>
2025-01-29 17:09:08 -0500

    ya casi

---
+++ Microservicio_incendios/app/Models/Fire.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fire extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cause','city_id','affected_area'];
+
+    public function cities(){
+
+        return $this->belongsTo(City::class);
+    }
+}
